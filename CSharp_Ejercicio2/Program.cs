aa29b994c6869196e0a0841f9b46447ddaadf397
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-21 17:39:55 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Ejercicio2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Pedir al usuario que ingrese 2 y decir cual es el mayo al usuario
+Console.WriteLine("Ingrese el primer número:");
+int numero4 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo número:");
+int numero5 = Convert.ToInt32(Console.ReadLine());
+
+if (numero4 > numero5)
+{
+    Console.WriteLine("El número " + numero4 + " es mayor que " + numero5);
+}
+else if (numero5 > numero4)
+{
+    Console.WriteLine("El número " + numero5 + " es mayor que " + numero4);
+}
+else
+{
+    Console.WriteLine("Los números son iguales.");
+}
